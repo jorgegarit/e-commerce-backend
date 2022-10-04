010be693b30d013b74efc6b9c05b2673c2dd3fe4
Jorge Garit <jorgegarit@gmail.com>
2022-10-03 22:53:26 -0400

    user can now create new tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -51,7 +51,15 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  // user can create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(error => {
+    console.log(error);
+    res.status(500).json(error);
+  });   
 });
 
 router.put('/:id', (req, res) => {
