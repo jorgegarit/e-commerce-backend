9129d102c75b32356d74f52de14f03ce24d74511
Jorge Garit <jorgegarit@gmail.com>
2022-10-03 22:09:41 -0400

    finished find category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,7 +5,7 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // Find All
+  // Find all the categories
   Category.findAll({
     include: {
 
@@ -14,12 +14,12 @@ router.get('/', (req, res) => {
 
       // these attributes will be based on the colums created form the product model
       attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
-      
+
     }
   })
   .then(dbCategoryData => {
     if(!dbCategoryData) {
-      res.status(404).json({ message: 'No Catergories were found' });
+      res.status(404).json({ message: 'No catergories were found' });
       return;
     }
     res.json(dbCategoryData);
@@ -31,8 +31,30 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  // find one category based on specific generated id 
+  Category.findOne({
+
+    // this decalres that we will be using a specific id 
+    where: {
+      id: req.params.id
+    },
+    // will be using the same model and attributes as the find all call
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'] 
+    }
+  })
+  .then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(404).json({ message: 'No catergories were found using that ID' });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json(error);
+  }); 
 });
 
 router.post('/', (req, res) => {
